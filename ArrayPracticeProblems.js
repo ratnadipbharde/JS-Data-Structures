47cff17d6f42b8c60bf29be576fd69ca9be1315c
ratnadipbharde <ratnadipbharde@gmail.com>
2023-01-10 01:08:27 +0530

    Added code to10 Random 3 Digit number

--- ArrayPracticeProblems.js
+++ ArrayPracticeProblems.js
@@ -1,1 +1,9 @@
-console.log("Data Structures")
+console.log("JS Data Structures")
+
+genrateThreeDigitRandomNumber(10);
+
+function genrateThreeDigitRandomNumber(number){
+    for (let index = 1; index <= number; index++) {
+        console.log(index+") "+ Math.floor(Math.random() * (999 - 100 + 1) + 100));  
+    }
+}
