1370c2c81b68c409b675a326ba1412468068fb6f
ratnadipbharde <ratnadipbharde@gmail.com>
2023-01-10 02:01:08 +0530

    Added code for store number in Array

--- ArrayPracticeProblems.js
+++ ArrayPracticeProblems.js
@@ -1,9 +1,34 @@
-console.log("JS Data Structures")
+console.log("JS Data Structures");
+console.log("Generates 10 Random 3 Digit number.")
+console.log(getRandomNumber(10));
+console.log("Store this random numbers into a array.")
+let arr=addIntoArray(10);
+console.log("Print Array.")
+printArray(arr);
 
-genrateThreeDigitRandomNumber(10);
-
-function genrateThreeDigitRandomNumber(number){
-    for (let index = 1; index <= number; index++) {
-        console.log(index+") "+ Math.floor(Math.random() * (999 - 100 + 1) + 100));  
+function getRandomNumber(number) {
+    let n = number;
+    for (let index = 1; index < n; index++) {
+        console.log(genrateThreeDigitRandomNumber())
     }
+}
+
+function genrateThreeDigitRandomNumber() {
+    let n = Math.floor(Math.random() * (999 - 100 + 1) + 100);
+    return n;
+}
+
+function addIntoArray(numberOfElement) {
+    let array = [];
+    for (let index = 0; index < numberOfElement; index++) {
+        let n = genrateThreeDigitRandomNumber();
+        array.push(n);
+    }
+    return array;
+}
+
+function printArray(array){
+  for (let index = 0; index < array.length; index++) {
+    console.log((index+1)+") "+array[index]);
+  }
 }
